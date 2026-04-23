1a185314f28ff65fe8a9730b35c5472937eaec96
NabilaPcr <nabila24si@mahasiswa.pcr.ac.id>
2026-04-23 15:55:22 +0700

    penggunaan link, pemanggilan tanpa render

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; // Import Router komponen
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./assets/tailwind.css";
+import Sidebar from "./layouts/Sidebar";
+import Header from "./layouts/Header";
+import Dashboard from "./Pages/Dashboard";
+import Orders from "./Pages/Orders";
+import Customers from "./Pages/Customers"; 
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Catatan: state currentPage mungkin tidak lagi diperlukan jika Sidebar menggunakan <Link>
+  const [currentPage, setCurrentPage] = useState("dashboard");
 
   return (
-    <> 
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        id="app-container"
+        className="bg-gray-100 min-h-screen flex font-barlow"
+      >
+        <Sidebar onNavigate={setCurrentPage} activePage={currentPage} />
+
+        <div
+          id="main-content"
+          className="flex-1 flex flex-col h-screen overflow-y-auto"
+        >
+          <Header />
+
+          <main className="flex-1 p-4">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/orders" element={<Orders />} />
+              <Route path="/customers" element={<Customers />} />
+            </Routes>
+          </main>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/layouts/Sidebar.jsx
@@ -0,0 +1,91 @@
+import {
+  FaThLarge,
+  FaShoppingBasket,
+  FaUsers,
+  FaPlus,
+  FaHome,
+  FaClipboardList,
+} from "react-icons/fa";
+import { Link, NavLink } from "react-router-dom";
+
+export default function Sidebar() {
+  const menuClass = ({ isActive }) =>
+  `flex items-center space-x-3 cursor-pointer rounded-xl p-4 font-medium transition ${
+    isActive 
+      ? 'bg-green-100 text-hijau' 
+      : 'text-gray-600 hover:bg-gray-50'
+  }`;
+  return (
+    <div
+      id="sidebar"
+      className="flex min-h-screen w-80 flex-col bg-white p-8 shadow-lg"
+    >
+      <div id="sidebar-logo" className="flex flex-col mb-10">
+        <span className="font-poppins text-[40px] text-gray-900 leading-none">
+          Sedap <b className="text-hijau">.</b>
+        </span>
+        <span className="font-semibold text-gray-400 text-xs">
+          Modern Admin Dashboard
+        </span>
+      </div>
+
+      <nav id="sidebar-menu" className="flex-1">
+        <ul className="space-y-2">
+          {/* Menu Dashboard */}
+          <li>
+            <NavLink to="/">
+              <div
+                className={menuClass}
+              >
+                <FaHome /> <span>Dashboard</span>
+              </div>
+            </NavLink>
+          </li>
+
+          {/* Menu Orders */}
+          <li>
+            <NavLink to="/orders">
+              <div
+                className={menuClass}
+              >
+                <FaClipboardList /> <span>Orders</span>
+              </div>
+            </NavLink>
+          </li>
+
+          {/* Menu Customers */}
+          <li>
+            <NavLink to="/customers">
+              <div
+                className={menuClass}
+              >
+                <FaUsers /> <span>Customers</span>
+              </div>
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <div id="sidebar-footer" className="mt-auto">
+        <div className="bg-hijau p-5 rounded-2xl shadow-lg mb-6 relative overflow-hidden">
+          <p className="text-white text-xs relative z-10 mb-4">
+            Please organize your menus through button below!
+          </p>
+          <button className="flex justify-center items-center w-full p-2 bg-white rounded-xl space-x-2 hover:bg-gray-100 transition relative z-10">
+            <FaPlus className="text-hijau" />
+            <span className="text-teks font-bold text-sm">Add Menus</span>
+          </button>
+          <div className="absolute -right-2 -bottom-2 opacity-20 text-white text-6xl rotate-12">
+            <FaClipboardList />
+          </div>
+        </div>
+        <p className="font-bold text-gray-400 text-xs">
+          Sedap Restaurant Admin
+        </p>
+        <p className="font-light text-gray-400 text-[10px]">
+          &copy; 2025 All Right Reserved
+        </p>
+      </div>
+    </div>
+  );
+}
